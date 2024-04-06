6f0ab6043f34c5caf6bea5ab3ae7cf7e926f4bbe
JVG-25 <65510976+JVG-25@users.noreply.github.com>
2024-04-05 19:23:32 -0600

    Update clase2.js

--- clase2.js
+++ clase2.js
@@ -1,11 +1,12 @@
-function calcularIndiceDeMasaCorporal(numero1, numero2, numero3){
-    const resultado = numero1 / (numero2*numero3);
+function calcularIndiceDeMasaCorporal(peso, estatura){
+    const resultado = peso / (estatura*estatura);
     
     if(resultado < 18.5){
         console.log("Bajo peso");
-    } else if( 18.5 < resultado > 24.9){
+    } 
+    else if(resultado >18.5 && resultado < 24.9){
         console.log("Peso saludable");
-    } else if(24.9 < resultado > 29.9){
+    } else if(resultado > 24.9 && resultado < 29.9){
         console.log("Sobrepeso");
     } else {
         console.log("Obesidad");
@@ -14,5 +15,7 @@ function calcularIndiceDeMasaCorporal(numero1, numero2, numero3){
     return resultado;
 }
 
-console.log (calcularIndiceDeMasaCorporal(106, 1.88, 1.88))
+console.log (calcularIndiceDeMasaCorporal(72, 1.64))
+
+
 
